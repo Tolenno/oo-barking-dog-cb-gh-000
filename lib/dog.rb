0800a8b26159467a027e8317ba92cf7fdf27db4e
Matt Hough <matt@hough.tv>
2017-09-10 21:20:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,10 +9,5 @@ class Dog
   def bark
     puts "woof!"
   end
-
 end
 
-define Dog
-write setter method for dog name
-write getter method for dog name
-write a method that puts woof! whenever called on a Dog instance
